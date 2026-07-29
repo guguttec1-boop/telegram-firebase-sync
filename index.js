86d6f86994ea9c32e20a97e6858111a559498e62
guguttec1-boop <guguttec1@gmail.com>
2026-07-29 09:22:46 +0300

    Refactor Telegram bot integration to use webhook

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const TelegramBot = require("node-telegram-bot-api");
 const admin = require("firebase-admin");
 
 const app = express();
+app.use(express.json());
 
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY)),
@@ -11,29 +11,44 @@ admin.initializeApp({
 
 const db = admin.database();
 
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
-  polling: true,
-});
-
-bot.on("channel_post", async (msg) => {
+app.post("/webhook", async (req, res) => {
   try {
+    const update = req.body;
+    const msg = update.channel_post;
+
+    if (!msg) {
+      return res.sendStatus(200);
+    }
+
     const post = {
       message_id: msg.message_id,
+      chat_id: msg.chat.id,
+      chat_title: msg.chat.title,
       text: msg.text || msg.caption || "",
       date: msg.date,
-      chat_title: msg.chat.title,
     };
+
+    if (msg.photo) {
+      post.photo = msg.photo[msg.photo.length - 1].file_id;
+    }
+
+    if (msg.video) {
+      post.video = msg.video.file_id;
+    }
 
     await db.ref("telegram_posts").push(post);
 
-    console.log("Saved post:", post);
+    console.log("Saved:", post);
+
+    res.sendStatus(200);
   } catch (err) {
     console.error(err);
+    res.sendStatus(500);
   }
 });
 
 app.get("/", (req, res) => {
-  res.send("Telegram Sync is running!");
+  res.send("Telegram Firebase Sync Running");
 });
 
 const PORT = process.env.PORT || 3000;
